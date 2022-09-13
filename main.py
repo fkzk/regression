c927f7fac28c341be1f4189c370cfaefc105b276
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-09-13 21:00:54 +0900

    メインの処理の部分のsin関数に即した命名を一般化した

--- main.py
+++ main.py
@@ -36,36 +36,36 @@ c_l2 = 0.005
 noise_rate = 0.05
 target_name = 'sin'
 
-# ターゲット関数の取得
+# ターゲット関数 t の取得
 target_fn = get_target_fn(target_name)
-# y = sin(πx)の計算
+# y = t(x)の計算
 x = jnp.linspace(-x_max, x_max, num=n_all)
-sinx = target_fn(x)
+target = target_fn(x)
 # サンプル点の選出
 random_key = jax.random.PRNGKey(random_seed)
 sample_x = jax.random.uniform(
     random_key, shape=(n_train,), minval=-x_max, maxval=x_max
 )
-# サンプル点に対する sin(πx_i)の計算
-sample_sinx = target_fn(sample_x)
+# サンプル点に対する t(x_i)の計算
+sample_target = target_fn(sample_x)
 # 学習用サンプルにノイズを加える
-sigma = (jnp.max(sinx) - jnp.min(sinx)) * noise_rate
+sigma = (jnp.max(target) - jnp.min(target)) * noise_rate
 _, noise_key = jax.random.split(random_key)
 noise = jax.random.normal(noise_key, shape=sample_x.shape) * sigma
-sample_sinx = sample_sinx + noise
+sample_target = sample_target + noise
 # サンプル点に対するx^pの計算
 p = jnp.arange(n_degree+1)
 X = jnp.power(sample_x[:, jnp.newaxis], p[jnp.newaxis, :])
 # 係数の決定
 I = jnp.eye(n_degree+1)
-a = jnp.linalg.inv(c_l2 * I + X.T @ X) @ X.T @ sample_sinx[:, jnp.newaxis]
+a = jnp.linalg.inv(c_l2 * I + X.T @ X) @ X.T @ sample_target[:, jnp.newaxis]
 # すべてのxに対する多項式の計算
 X_all = jnp.power(x[:, jnp.newaxis], p[jnp.newaxis, :])
 poly_x = (X_all @ a)[:]
 
 # 評価値の計算・表示
-mae = jnp.mean(jnp.abs(sinx-poly_x))
-nmae = mae / (jnp.max(sinx) - jnp.min(sinx))
+mae = jnp.mean(jnp.abs(target-poly_x))
+nmae = mae / (jnp.max(target) - jnp.min(target))
 score = - 20 * jnp.log10(nmae)
 print(f'近似スコア: {score:.2f} dB')
 
@@ -73,21 +73,23 @@ print(f'近似スコア: {score:.2f} dB')
 fig = plt.Figure()
 ax = fig.add_subplot(1, 1, 1, xlabel='x', ylabel='y')
 # グラフの見た目の設定
-ax.set_title('$y = \sin (\pi x)$')
+ax.set_title(f'$y = ${target_name}$ (x)$')
 ax.axhline(0, color='#777777')
 ax.axvline(0, color='#777777')
 ax.tick_params(direction='in', bottom=True, top=True, left=True, right=True)
 # 表示範囲の設定
 ax.set_xlim(-x_max, x_max)
-y_min = jnp.min(sinx)
-y_max = jnp.max(sinx)
+y_min = jnp.min(target)
+y_max = jnp.max(target)
 ylim_min = y_min - (y_max-y_min) * 0.15
 ylim_max = y_max + (y_max-y_min) * 0.15
 ax.set_ylim(ylim_min, ylim_max)
 # グラフのプロット
-ax.plot(x, sinx, label='target')
-ax.scatter(sample_x, sample_sinx, color='red', zorder=2, label='training sample')
+ax.plot(x, target, label='target')
+ax.scatter(
+    sample_x, sample_target, color='red', zorder=2, label='training sample'
+)
 ax.plot(x, poly_x, label=f'predicted ($d={n_degree}, \lambda={c_l2}$)')
 # 凡例の表示・図の出力
 ax.legend()
-fig.savefig('sinx.png')
+fig.savefig('result.png')
