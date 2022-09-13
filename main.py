cb0b514e330c9ba9d3fd51c7af06f5b90bd73fbf
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-09-13 15:04:46 +0900

    ランダムにサンプルした10点についてsin(πx)を表示

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
@@ -10,4 +11,8 @@ ax.tick_params(direction='in', bottom=True, top=True, left=True, right=True)
 x = jnp.linspace(-1, 1, num=101)
 sinx = jnp.sin(jnp.pi*x)
 ax.plot(x, sinx)
+random_key = jax.random.PRNGKey(0)
+sample_x = jax.random.uniform(random_key, shape=(10,), minval=-1, maxval=1)
+sample_sinx = jnp.sin(jnp.pi*sample_x)
+ax.scatter(sample_x, sample_sinx, color='red', zorder=2)
 fig.savefig('sinx.png')
