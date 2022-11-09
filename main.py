e72245136efe4e445467c14e4a6e15648e0b1002
Fukuzaki Shota <fukuzaki@tkhm.elec.keio.ac.jp>
2022-11-09 22:01:40 +0900

    小区間に分割する関数を実装した
    ただし、高速化はできていない

--- main.py
+++ main.py
@@ -15,6 +15,20 @@ def main():
     d = 7
     p = np.arange(d+1)[np.newaxis, :]
     eps = 1e-8
+    n_split = 16
+    def quantitize(xs, ys, alpha=-1, beta=1):
+        x_split = np.linspace(alpha, beta, n_split+1)[:, np.newaxis]
+        x_filter = np.where(x_split > xs.T, 1, 0)
+        y_map = x_filter[1:, :] - x_filter[:-1, :]
+        y_map_sum = np.sum(y_map, axis=1, keepdims=True) 
+        if np.all(y_map_sum):
+            y_map = y_map / y_map_sum
+            xs = (x_split[:-1] + x_split[1:]) / 2
+            ys = y_map @ ys
+        return xs, ys
+    def non_quantitize(xs, ys):
+        return xs, ys
+
     for i_N, N in enumerate(Ns):
         sample_x = np.random.uniform(-1, 1, (N, n_repeat))
         sample_noise = np.random.normal(0, y_range*0.05, (N, n_repeat) )
@@ -23,9 +37,12 @@ def main():
             # 多項式フィッティング
             ## 学習サンプルから係数を求める
             t_start = time.time() # 測定開始
-            sample_X = sample_x[:, [i_repeat]] ** p
+            _sample_x, _sample_y = quantitize(
+                sample_x[:, [i_repeat]], sample_y[:, [i_repeat]]
+            )
+            sample_X = _sample_x ** p
             sample_XX_inv = np.linalg.inv(sample_X.T @ sample_X)
-            a =  sample_XX_inv @ sample_X.T @ sample_y[:, [i_repeat]]
+            a =  sample_XX_inv @ sample_X.T @ _sample_y
             t_end = time.time() # 測定終了
             ## 求めた係数を用いて y の値を予測
             X = x[:, np.newaxis] ** p
