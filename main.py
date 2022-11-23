aeab7c27edc0df3ad9f05449eb226bed33ed35e6
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-11-23 21:54:55 +0900

    スコア計算、グラフ出力を関数化した

--- main.py
+++ main.py
@@ -2,6 +2,37 @@ from matplotlib.figure import Figure
 import japanize_matplotlib as _
 import numpy as np
 
+def calculate_score(y, y_pred, score_eps):
+    norm_y = np.sum(np.abs(y))
+    norm_diff = np.sum(np.abs(y - y_pred))
+    score = norm_diff / (norm_y + score_eps)
+    return score
+
+def save_graph(
+    xy = None,
+    xy_sample = None,
+    xy_pred = None,
+    title = None,
+    filename = 'out.png',
+):
+    fig = Figure()
+    ax = fig.add_subplot(1, 1, 1, xlabel='$x$', ylabel='$y$')
+    if title is not None:
+        ax.set_title(title)
+    ax.axhline(0, color='#777777')
+    ax.axvline(0, color='#777777')
+    if xy is not None:
+        x, y = xy
+        ax.plot(x, y, label='真の関数 $f$')
+    if xy_sample is not None:
+        x_sample, y_sample = xy_sample
+        ax.scatter(x_sample, y_sample, color='red', label='学習サンプル')
+    if xy_pred is not None:
+        x_pred, y_pred = xy_pred
+        ax.plot(x_pred, y_pred, label=r'回帰関数 $\hat{f}$')
+    ax.legend()
+    fig.savefig(filename)
+
 def main():
     # 実験条件
     x_min = -1
@@ -29,21 +60,13 @@ def main():
     X = x[:, np.newaxis] ** p[np.newaxis, :]
     y_pred = np.squeeze(X @ a)
     # 評価指標の算出
-    norm_y = np.sum(np.abs(y))
-    norm_diff = np.sum(np.abs(y - y_pred))
-    score = norm_diff / (norm_y + score_eps)
+    score = calculate_score(y, y_pred, score_eps)
     print(f'{score = :.3f}')
     # グラフの表示
-    fig = Figure()
-    ax = fig.add_subplot(1, 1, 1, xlabel='$x$', ylabel='$y$')
-    ax.set_title(r'$y = \sin (\pi x)$')
-    ax.axhline(0, color='#777777')
-    ax.axvline(0, color='#777777')
-    ax.plot(x, y, label='真の関数 $f$')
-    ax.scatter(sample_x, sample_y, color='red', label='学習サンプル')
-    ax.plot(x, y_pred, label=r'回帰関数 $\hat{f}$')
-    ax.legend()
-    fig.savefig('out.png')
+    save_graph(
+        xy=(x, y), xy_sample=(sample_x, sample_y), xy_pred=(x, y_pred),
+        title=r'$y = \sin (\pi x)$'
+    )
 
 if __name__ == '__main__':
     main()
