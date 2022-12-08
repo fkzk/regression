0771cd32e4c5f0001d7243659324af9e2e96d829
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-12-08 10:57:24 +0900

    多項式フィッティングをPolyRegressorに集約した

--- main.py
+++ main.py
@@ -35,6 +35,25 @@ def save_graph(
     ax.legend()
     fig.savefig(filename)
 
+class PolyRegressor:
+    def __init__(self, d):
+        self.d = d
+        self.p = np.arange(d+1)[np.newaxis, :]
+
+    def fit(self, x_sample, y_sample):
+        ## Xを作る
+        x_s = x_sample[:, np.newaxis]
+        X_s = x_s ** self.p
+        ## 係数aを求める
+        y_s = y_sample[:, np.newaxis]
+        X_inv = np.linalg.inv(X_s.T @ X_s)
+        self.a = X_inv @ X_s.T @ y_s
+
+    def predict(self, x):
+        ## yの予測値を計算
+        y_pred = np.squeeze((x[:, np.newaxis] ** self.p) @ self.a)
+        return y_pred
+
 def main():
     # 実験条件
     x_min = -1
@@ -45,6 +64,7 @@ def main():
     eps_score = 1e-8
     # 多項式フィッティングの設定
     d = 3
+    regressor = PolyRegressor(d)
     # x, f(x)の準備
     x = np.linspace(x_min, x_max, n_test)
     y = np.sin(np.pi * x)
@@ -54,16 +74,8 @@ def main():
     noise_sample = np.random.normal(0, range_y*noise_ratio, (n_train, ))
     y_sample = np.sin(np.pi * x_sample) + noise_sample
     # 多項式フィッティング
-    ## Xを作る
-    p = np.arange(d+1)[np.newaxis, :]
-    x_s = x_sample[:, np.newaxis]
-    X_s = x_s ** p
-    ## 係数aを求める
-    y_s = y_sample[:, np.newaxis]
-    X_inv = np.linalg.inv(X_s.T @ X_s)
-    a = X_inv @ X_s.T @ y_s
-    ## yの予測値を計算
-    y_pred = np.squeeze((x[:, np.newaxis] ** p) @ a)
+    regressor.fit(x_sample, y_sample)
+    y_pred = regressor.predict(x)
     # 評価指標の計算
     score = calculate_score(y, y_pred, eps_score)
     print(f'{score = :.3f}')
